67a11e5ced5ee2a92bf90cab0e9ee949f6343014
Kazimierz Nowak <streamigry@gmail.com>
2026-05-31 10:28:24 +0200

    Migrate to PostgreSQL and orchestrate with Docker Compose

--- src/database.py
+++ src/database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./ecotrans.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://eco_user:eco_password@db:5432/ecotrans_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 sessionLocal = sessionmaker(autocommit= False, autoflush= False, bind=engine)
 Base = declarative_base()
