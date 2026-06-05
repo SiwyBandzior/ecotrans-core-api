818a56e7036a05d03d1a8ed2730911a63c569942
Kazimierz Nowak <streamigry@gmail.com>
2026-06-05 20:58:59 +0200

    Add JWT authentication and bcrypt password hashing

--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing import Optional
 from sqlalchemy.orm import Session
 
-from . import models
+from . import models, auth
 from .database import engine, sessionLocal
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(
-    title = "EcoTrans Core Api"
-)
-
+app = FastAPI(title="EcoTrans Core API - Secured")
 
 class TechnicianTask(BaseModel):
     barcode: str
     technician_name: str
     is_hazardous: bool
     temperature_celsius: Optional[float] = None
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
 
 
 def get_db():
@@ -27,9 +29,44 @@ def get_db():
     finally:
         db.close()
 
+async def get_current_user(token: str = Depends(auth.oauth2_scheme), db: Session= Depends(get_db)):
+    try:
+        payload = auth.jwt.decode(token, auth.SECRET_KEY, algorithms=[auth.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except auth.jwt.JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+    user = db.query(models.UserDB).filter(models.UserDB.username == username).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
 
-@app.post("/tasks/")
-async def create_task(task: TechnicianTask, db: Session = Depends(get_db)):
+
+@app.post("/register")
+async def register_user(user:UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.UserDB).filter(models.UserDB.username == user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    
+    hashed_pw = auth.get_password_hash(user.password)
+    new_user = models.UserDB(username=user.username, hashed_password=hashed_pw)
+    db.add(new_user)
+    db.commit()
+    return {"message": "User created successfully"}
+
+@app.post("/login/")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.UserDB).filter(models.UserDB.username == form_data.username).first()
+    if not user or not auth.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    
+    access_token = auth.create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/task/")
+async def create_task(task: TechnicianTask, db: Session = Depends(get_db), current_user: models.UserDB = Depends(get_current_user)):
     db_task = db.query(models.TaskDB).filter(models.TaskDB.barcode == task.barcode).first()
     if db_task:
         raise HTTPException(status_code=400, detail="Barcode already registered.")
@@ -40,10 +77,10 @@ async def create_task(task: TechnicianTask, db: Session = Depends(get_db)):
         is_hazardous = task.is_hazardous,
         temperature_celsius = task.temperature_celsius
     )
+
     db.add(new_task)
     db.commit()
-    db.refresh(new_task)
-    return{"message": "Task saved to DB!", "task_id": new_task.id}
+    return {"message": f"Task saved securely by {current_user.username}"}
 
 
 @app.get("/tasks/{barcode}")
--- src/models.py
+++ src/models.py
@@ -9,3 +9,11 @@ class TaskDB(Base):
     technician_name = Column(String)
     is_hazardous = Column(Boolean, default=False)
     temperature_celsius = Column(Float, nullable=True)
+
+
+class UserDB(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
